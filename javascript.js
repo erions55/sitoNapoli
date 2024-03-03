6e3b9261a58505a610d6dd33a0761ef695bc573a
VueKo <gabriele.resconi@gmail.com>
2024-03-03 08:13:39 +0100

    laaaaaal

---
+++ javascript.js
@@ -0,0 +1,16 @@
+//Resconi Gabriele 4AI javascript.js
+
+
+function display(id) {
+
+    let opz = id.style.display;
+
+    if(opz == "none")
+        id.style.display = "block";
+    else
+        id.style.display = "none";
+}
+
+
+
+
